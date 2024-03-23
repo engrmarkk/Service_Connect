a4dde92c96416480d110e667a6646f64485a1d48
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-03-23 20:37:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -27,8 +27,14 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     profile_picture = db.Column(db.Text)
+    phone = db.Column(db.String(20), nullable=True)
+    country = db.Column(db.String(120), nullable=True)
+    state = db.Column(db.String(120), nullable=True)
+    local_government = db.Column(db.String(120), nullable=True)
+    otp = db.Column(db.String(6), nullable=True)
     is_admin = db.Column(db.Boolean, default=False)
     is_worker = db.Column(db.Boolean, default=False)
+    otp_verified = db.Column(db.Boolean, default=False)
     date_added = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
     # relationship
@@ -38,6 +44,12 @@ class Users(db.Model, UserMixin):
 class WorkerProfile(db.Model, UserMixin):
     __tablename__ = "worker_profile"
     id = db.Column(db.String(120), primary_key=True, default=generate_hex, nullable=False)
+    company = db.Column(db.String(180), nullable=True)
+    description = db.Column(db.String(120), nullable=True)
+    rate = db.Column(db.Float, nullable=True)
+    work_pic1 = db.Column(db.Text)
+    work_pic2 = db.Column(db.Text)
+    work_pic3 = db.Column(db.Text)
     service_offered = db.Column(db.ForeignKey("services.id"))
     user = db.Column(db.ForeignKey("users.id"))
 
