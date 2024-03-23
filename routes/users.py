ea3312600757420f54b18c0486e4f0e22ff2e152
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-03-23 20:37:38 +0100

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, redirect, render_template, url_for
 from models import save_services, get_services
+from flask_login import login_required
+from decorator import user_required
 
 
 user = Blueprint("user", __name__)
