457af088951ac50f499f747359ff3bec3dddb687
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-03-23 16:31:56 +0100

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_migrate import Migrate
+# from flask_session import Session
+# from flask_moment import Moment
+
+
+db = SQLAlchemy()
+app = Flask(__name__)
+mail = Mail()
+login_manager = LoginManager()
+migrate = Migrate()
+# sess = Session()
+# moment = Moment()
