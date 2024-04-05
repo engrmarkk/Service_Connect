813a0f6688b09e50bd4ffb64b98cedaf28a9d113
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-04-06 00:12:20 +0100

    Update worker.py

--- routes/worker.py
+++ routes/worker.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, session
-from flask_login import login_user, logout_user, logout_user, login_required
+from flask_login import login_user, logout_user, logout_user, login_required, current_user
 from models import WorkerProfile
 from decorator import worker_required
 
