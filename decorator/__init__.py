47f21cde0391769e3500149970cf7a08a67fff39
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-18 22:20:06 +0100

    Update users.py

--- decorator/__init__.py
+++ decorator/__init__.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask_login import current_user
+from flask import redirect, url_for, abort
 
 
 def worker_required(f):
@@ -19,4 +20,17 @@ def user_required(f):
             return f(*args, **kwargs)
         else:
             return abort(403)
+    return decorated_function
+
+
+# complete profile
+def complete_profile(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated:
+            if current_user.is_worker and not current_user.worker_profile:
+                return redirect(url_for("worker.update_profile"))
+            return f(*args, **kwargs)
+        else:
+            return f(*args, **kwargs)
     return decorated_function
