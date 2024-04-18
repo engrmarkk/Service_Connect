19fc4aa75993a10c646849124191e05f44f0a64e
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-18 22:20:09 +0100

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, redirect, render_template, url_for, session
 from models import save_services, get_services
-from flask_login import login_required
+from flask_login import login_required, current_user
 from decorator import user_required
+from decorator import complete_profile
 
 
 user = Blueprint("user", __name__)
 
 
 @user.route("/")
+@complete_profile
 def home():
     # save_services()
     alert = session.pop("alert", None)
